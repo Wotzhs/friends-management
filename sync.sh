35f73829df1750ff0dc94eba1d422897c6ba4443
Wotzhs <wotzhs@gmail.com>
2018-07-22 22:41:31 +0800

    delete sync.sh
    remove work-station specific sync script

--- sync.sh
+++
@@ -1,1 +0,0 @@
-unisonText C:/Users/sean/work/src/friends_management -noupdate="C:/Users/sean/work/src/friends_management" -prefer=newer -repeat=watch -ignore "Regex node_modules|.git|public/assets|vendor" ssh://lb//home/sean/work/src/friends_management
